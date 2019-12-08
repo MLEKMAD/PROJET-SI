33beba110f7a8127aa328e11451bf0095595c749
hatimoss <hatimlahlali.13@outlook.fr>
2019-12-08 16:15:32 +0100

    adding auth

--- back_end/index.js
+++ back_end/index.js
@@ -7,12 +7,18 @@ const app            = express();
 const authentification= require('./routes/users/authentification');
 
 app.use(bodyParser.json());
-// app.use((req,res,next)=>{
-  // res.setHeader('Access-Control-Allow-Origin','*');
-  // res.setHeader('Access-Control-Allow-Methods','OPTIONS,GET,POST,PUT,PATCH,DELETE');
+app.use(
+  bodyParser.urlencoded({
+    extended:true
+  })
+);
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','OPTIONS,GET,POST,PUT,PATCH,DELETE');
   // res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
+  next();
 
-// });
+});
 
 app.use(authentification);
 
--- back_end/routes/users/authentification.js
+++ back_end/routes/users/authentification.js
@@ -3,7 +3,7 @@ const router=express.Router();
 
 router.get("/authentification",(req,res,next)=>{
     res.status(201).json({
-        message:'first get',
+        message:'get authentification',
         get:[{
             id_user:1,
             full_name:'hatim lahlali',
@@ -14,7 +14,24 @@ router.get("/authentification",(req,res,next)=>{
         done:'true' 
                 });
                     });
+router.post("/authentification",(req,res,next)=>{
+    const full_name=req.body.full_name;
+    const password_hashed=req.body.password;
+    const email_user=req.body.email_user;
+    const phone_number_user=req.body.phone_number_user;
+//compare with oracle bdd
+    console.log(req.body);
+    res.status(201).json({
+        message:'POST authentification',
+        full_name,
+        password_hashed,
+        email_user,
+        phone_number_user,
+        done:'done succesfully'
+    })
+//******************* */
 
+})
 
                     
 
