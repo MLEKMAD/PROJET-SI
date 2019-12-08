7a7c248ef6106a699a0e771b597c27a53876c716
hatimoss <hatimlahlali.13@outlook.fr>
2019-12-08 19:06:44 +0100

    validator added

--- back_end/controllers/user.js
+++ back_end/controllers/user.js
@@ -1,4 +1,5 @@
 
+const validatePhoneNumber = require('validate-phone-number-node-js');
 
 
 exports.getuser=(req,res,next)=>{
@@ -9,20 +10,37 @@ exports.getuser=(req,res,next)=>{
 };
 
 exports.postuser=(req,res,next)=>{
-    const full_name=req.body.full_name;
-    const password_hashed=req.body.password;
-    const email_user=req.body.email_user;
-    const phone_number_user=req.body.phone_number_user;
-//compare with oracle bdd if existe 
-    res.status(201).json({
-        message:'POST user',
-        full_name,
-        password_hashed,
-        email_user,
-        phone_number_user,
-        done:'done succesfully'
-    })
-//******************* */
+
+    req.check('full_name','full name umpty').notEmpty();
+    req.check('password_hashed','password umpty').isLength({ min: 6 });
+    req.check('email','email not valid').isEmail();
+    req.check('phone_number_user','number not valide').isLength({min:10})
+    const phone_number_valide = validatePhoneNumber.validate(req.body.phone_number_user);
+    if(!phone_number_valide){
+        res.status(401).json({
+            message:'phone number not valide'
+        })
+    }
+    // try nd catch error 
+    var errors=req.validationErrors();
+    if(errors){
+        res.status(401).json({
+            message:'data not valide user post'
+        })
+    }
+    else{
+        // const full_name=req.body.full_name;
+        // const password_hashed=req.body.password;
+        // const email_user=req.body.email_user;
+    //compare with oracle bdd if existe 
+    //******************* */
+
+        res.status(201).json({
+            message:'POST user',
+            done:'done succesfully'
+        })
+
+    }
 };
 
 exports.updateuser=(req,res,next)=>{
--- back_end/index.js
+++ back_end/index.js
@@ -2,7 +2,7 @@ const express        = require('express');
 // const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const app            = express();
-
+const validator =require('express-validator');
 
 const user= require('./routes/users/user');
 const idea= require('./routes/ideas/idea');
@@ -21,6 +21,7 @@ app.use((req,res,next)=>{
   next();
 
 });
+app.use(validator());
 
 app.use(user);
 app.use(idea);
